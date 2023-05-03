38a0485da714985124e498ba17f5b3d8c14df177
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-03 16:51:36 +0200

    Add quick sort.

---
+++ quicksort/quicksort_test.go
@@ -0,0 +1,13 @@
+package quicksort
+
+import (
+	"testing"
+
+	"github.com/tel22a-inf/sorting/testhelpers"
+)
+
+func TestQuickSort(t *testing.T) {
+	list := []int{42, 2, 13, 12, 5, 6, 23, 25, 38, 77, 200, 1, 0}
+	QuickSort(list)
+	testhelpers.AssertListsEqual(t, []int{0, 1, 2, 5, 6, 12, 13, 23, 25, 38, 42, 77, 200}, list)
+}
