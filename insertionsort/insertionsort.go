2425e88c862061aff94626a9171383350f4afa51
Reiner Hüchting <dev@reinerhuechting.de>
2024-01-26 14:26:12 +0100

    Insertionsort hinzugefügt.

--- insertionsort/insertionsort.go
+++ insertionsort/insertionsort.go
@@ -2,5 +2,7 @@ package insertionsort
 
 // InsertionSort sorts the given list using the insertion sort algorithm.
 func InsertionSort(list []int) {
-	// TODO
+	for pos := 0; pos < len(list); pos++ {
+		MoveLeft(list, pos)
+	}
 }
--- insertionsort/moveleft.go
+++ insertionsort/moveleft.go
@@ -2,5 +2,7 @@ package insertionsort
 
 // MoveLeft moves the element at the given position to the left until it is in the correct position.
 func MoveLeft(list []int, pos int) {
-	// TODO
+	for i := pos; i > 0 && list[i] < list[i-1]; i-- {
+		list[i-1], list[i] = list[i], list[i-1]
+	}
 }
